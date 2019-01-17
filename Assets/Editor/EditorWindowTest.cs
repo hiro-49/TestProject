06551f26b3d9ebc9d94ac03d19bc59aa949d7771
hiro-49 <kazuki97101@gmail.com>
2019-01-17 21:54:21 +0900

    エディタ拡張練習
    EditorWindowTestを追加

---
+++ Assets/Editor/EditorWindowTest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorWindowTest : EditorWindow{
+	[MenuItem("Editor/Sample")]
+	private static void Create(){
+		//生成
+		GetWindow<EditorWindowTest>("サンプル");
+	}
+}
